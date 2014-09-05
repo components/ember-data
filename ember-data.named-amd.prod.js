5acf83a10e2a56dbe149234e3d2e14c2e4aad31d
Tomster <tomster@emberjs.com>
2014-09-05 07:13:51 +0000

    Ember Data Bower Auto build for https://github.com/emberjs/data/commits/b819efbf5fecfc6c0145d6b0682cf7b283fc4482.

--- ember-data.named-amd.prod.js
+++ ember-data.named-amd.prod.js
@@ -1794,11 +1794,11 @@ define("ember-data/core",
       /**
         @property VERSION
         @type String
-        @default '1.0.0-beta.10+canary.9b3af93eb2'
+        @default '1.0.0-beta.10+canary.b819efbf5f'
         @static
       */
       DS = Ember.Namespace.create({
-        VERSION: '1.0.0-beta.10+canary.9b3af93eb2'
+        VERSION: '1.0.0-beta.10+canary.b819efbf5f'
       });
 
       if (Ember.libraries) {
