84a21a376495cd16c206e2b69cd3e463c209e4da
Tomster <tomster@emberjs.com>
2014-09-05 03:30:43 +0000

    Ember Data Bower Auto build for https://github.com/emberjs/data/commits/9d3d2103d009aa483d209356c8acf1eac6c88ad1.

--- ember-data.named-amd.prod.js
+++ ember-data.named-amd.prod.js
@@ -1791,11 +1791,11 @@ define("ember-data/core",
       /**
         @property VERSION
         @type String
-        @default '1.0.0-beta.10+canary.95f6d29e74'
+        @default '1.0.0-beta.10+canary.9d3d2103d0'
         @static
       */
       DS = Ember.Namespace.create({
-        VERSION: '1.0.0-beta.10+canary.95f6d29e74'
+        VERSION: '1.0.0-beta.10+canary.9d3d2103d0'
       });
 
       if (Ember.libraries) {
@@ -9135,14 +9135,16 @@ define("ember-data/system/relationships/has_many",
       notifyHasManyAdded: function(key, record, idx) {
         var relationship = this._relationships[key];
         var manyArray = relationship.manyArray;
-        //TODO(Igor) double check with yehuda whether this is the correct method
         manyArray.addRecord(record, idx);
+        //We need to notifyPropertyChange in the adding case because we need to make sure
+        //we fetch the newly added record in case it is unloaded
+        //TODO(Igor): Consider whether we could do this only if the record state is unloaded
+        this.notifyPropertyChange(key);
       },
 
       notifyHasManyRemoved: function(key, record) {
         var relationship = this._relationships[key];
         var manyArray = relationship.manyArray;
-        //TODO(Igor) double check with yehuda whether this is the correct method
         manyArray.removeRecord(record);
       }
     });
