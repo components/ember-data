0b40f360e6649eae4e6a5b177437866d51cec109
Tomster <tomster@emberjs.com>
2014-11-03 20:09:41 +0000

    Ember Data Bower Auto build for https://github.com/emberjs/data/commits/300904876d1851fd8b90ca78742e774ae83b69d3.

--- ember-data.js
+++ ember-data.js
@@ -10026,9 +10026,9 @@ enifed("ember-data/system/relationships/relationship",
 
     BelongsToRelationship.prototype._super$removeRecordFromOwn = Relationship.prototype.removeRecordFromOwn;
     BelongsToRelationship.prototype.removeRecordFromOwn = function(record) {
-      if (!this.members.has(record)){ return;}
-      this._super$removeRecordFromOwn(record);
+      if (!this.members.has(record)) { return; }
       this.inverseRecord = null;
+      this._super$removeRecordFromOwn(record);
     };
 
     BelongsToRelationship.prototype.findRecord = function() {
