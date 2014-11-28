37c0e8ba01f688ef0dcb680636d884ba0ba040cc
Tomster <tomster@emberjs.com>
2014-11-28 09:20:56 +0000

    Ember Data Bower Auto build for https://github.com/emberjs/data/commits/ac4213564ced393e84587a91d7a11c8bd292a924.

--- ember-data.named-amd.js
+++ ember-data.named-amd.js
@@ -6380,7 +6380,9 @@ define("ember-data/system/model/model",
       toJSON: function(options) {
         if (!JSONSerializer) { JSONSerializer = requireModule("ember-data/serializers/json_serializer")["default"]; }
         // container is for lazy transform lookups
-        var serializer = JSONSerializer.create({ container: this.container });
+        var serializer = JSONSerializer.create({
+          container: this.container
+        });
         return serializer.serialize(this, options);
       },
 
@@ -6435,8 +6437,6 @@ define("ember-data/system/model/model",
         this._data = this._data || {};
         return this._data;
       }).readOnly(),
-
-      _data: null,
 
       init: function() {
         this._super();
--- ember-data.named-amd.prod.js
+++ ember-data.named-amd.prod.js
@@ -6358,7 +6358,9 @@ define("ember-data/system/model/model",
       toJSON: function(options) {
         if (!JSONSerializer) { JSONSerializer = requireModule("ember-data/serializers/json_serializer")["default"]; }
         // container is for lazy transform lookups
-        var serializer = JSONSerializer.create({ container: this.container });
+        var serializer = JSONSerializer.create({
+          container: this.container
+        });
         return serializer.serialize(this, options);
       },
 
@@ -6413,8 +6415,6 @@ define("ember-data/system/model/model",
         this._data = this._data || {};
         return this._data;
       }).readOnly(),
-
-      _data: null,
 
       init: function() {
         this._super();
