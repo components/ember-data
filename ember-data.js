3f7064a02a1f2c617756c40e6c2c26595c24d3ca
Tomster <tomster@emberjs.com>
2015-04-17 19:43:11 +0000

    Ember Data Bower Auto build for https://github.com/emberjs/data/commits/219b33173810043811bf2a2ff70c6a934f8a878f.

--- ember-data.js
+++ ember-data.js
@@ -4608,7 +4608,7 @@
     }
     var activemodel$adapter$lib$setup$container$$default = activemodel$adapter$lib$setup$container$$setupActiveModelAdapter;
     var ember$data$lib$core$$DS = Ember.Namespace.create({
-      VERSION: '1.0.0-beta.17+canary.65f003aace'
+      VERSION: '1.0.0-beta.17+canary.219b331738'
     });
 
     if (Ember.libraries) {
