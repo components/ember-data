0440cd46329d71c9f12e4008c02329725f31d93c
Tomster <tomster@emberjs.com>
2015-06-02 21:24:25 +0000

    Ember Data Bower Auto build for https://github.com/emberjs/data/commits/303415ff349865f570a6c16c4c4d98343121e367.

--- ember-data-tests.js
+++ ember-data-tests.js
@@ -19601,6 +19601,55 @@ define(
 
 
 define(
+  "ember-data/tests/unit/store/get-by-id-test",
+  ["exports"],
+  function(__exports__) {
+    "use strict";
+
+    function __es6_export__(name, value) {
+      __exports__[name] = value;
+    }
+
+    var env, store, Person;
+    var run = Ember.run;
+
+    module('unit/store/getById - Store getById', {
+      setup: function () {
+
+        Person = DS.Model.extend();
+        Person.toString = function () {
+          return 'Person';
+        };
+
+        env = setupStore({
+          person: Person
+        });
+        store = env.store;
+      },
+
+      teardown: function () {
+        Ember.run(store, 'destroy');
+      }
+    });
+
+    test('getById should return the record if it is in the store ', function () {
+
+      run(function () {
+        var person = store.push('person', { id: 1 });
+        equal(person, store.getById('person', 1), 'getById only return the corresponding record in the store');
+      });
+    });
+
+    test('getById should return null if the record is not in the store ', function () {
+      run(function () {
+        equal(null, store.getById('person', 1), 'getById returns null if the corresponding record is not in the store');
+      });
+    });
+  }
+);
+
+
+define(
   "ember-data/tests/unit/store/has_record_for_id_test",
   ["exports"],
   function(__exports__) {
@@ -21660,6 +21709,13 @@ if (!QUnit.urlParams.nojshint) {
 module('JSHint - ember-data/tests/unit/store');
 test('ember-data/tests/unit/store/create-record-test.js should pass jshint', function() { 
   ok(true, 'ember-data/tests/unit/store/create-record-test.js should pass jshint.'); 
+});
+
+}
+if (!QUnit.urlParams.nojshint) {
+module('JSHint - ember-data/tests/unit/store');
+test('ember-data/tests/unit/store/get-by-id-test.js should pass jshint', function() { 
+  ok(true, 'ember-data/tests/unit/store/get-by-id-test.js should pass jshint.'); 
 });
 
 }
