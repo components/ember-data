a0ce275402ced613a1cc0c355bedd8742b984246
Tomster <tomster@emberjs.com>
2015-08-18 19:36:36 +0000

    Ember Data Bower Auto build for https://github.com/emberjs/data/commits/64bc46fafb3cc32972cf0a441fa510cb2801a594.

--- ember-data.js
+++ ember-data.js
@@ -1391,12 +1391,12 @@
       import DS from 'ember-data';
 
       export default DS.RESTAdapter.extend({
-        headers: function() {
+        headers: Ember.computed(function() {
           return {
             "API_KEY": Ember.get(document.cookie.match(/apiKey\=([^;]*)/), "1"),
             "ANOTHER_HEADER": "Some header value"
           };
-        }.property().volatile()
+        }).volatile()
       });
       ```
 
