b8218ae102c14518ee53cfbcdce01005c9459592
Tomster <tomster@emberjs.com>
2016-01-20 05:25:47 +0000

    Ember Data Bower Auto build for https://github.com/emberjs/data/commits/73e4f3f8b1aa981a79433f2a2e1d0dd68a6122ff.

--- ember-data.prod.js
+++ ember-data.prod.js
@@ -6,7 +6,7 @@
  * @copyright Copyright 2011-2016 Tilde Inc. and contributors.
  *            Portions Copyright 2011 LivingSocial Inc.
  * @license   Licensed under MIT license (see license.js)
- * @version   2.5.0-canary+cf0bce175f
+ * @version   2.5.0-canary+73e4f3f8b1
  */
 
 var define, requireModule, require, requirejs;
@@ -12051,9 +12051,9 @@ define('ember-data/adapters/rest', ['exports', 'ember', 'ember-data/adapter', 'e
     },
 
     /**
-      Called by the store in order to fetch a JSON array for
-      the unloaded records in a belongs-to relationship that were originally
-      specified as a URL (inside of `links`).
+      Called by the store in order to fetch the JSON for the unloaded record in a
+      belongs-to relationship that was originally specified as a URL (inside of
+      `links`).
        For example, if your original payload looks like this:
        ```js
       {
@@ -15293,7 +15293,7 @@ define('ember-data/transform', ['exports', 'ember'], function (exports, _ember) 
   });
 });
 define("ember-data/version", ["exports"], function (exports) {
-  exports.default = "2.5.0-canary+cf0bce175f";
+  exports.default = "2.5.0-canary+73e4f3f8b1";
 });
 define("ember-inflector", ["exports", "ember", "ember-inflector/lib/system", "ember-inflector/lib/ext/string"], function (exports, _ember, _emberInflectorLibSystem, _emberInflectorLibExtString) {
 
