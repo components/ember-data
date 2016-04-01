9520ee784224335ef13999b28580245c392d481c
Tomster <tomster@emberjs.com>
2016-04-01 20:09:10 +0000

    Ember Data Bower Auto build for https://github.com/emberjs/data/commits/ad7c26c60a48135f5cddb4506f8e624a78988ccf.

--- ember-data.js
+++ ember-data.js
@@ -6,7 +6,7 @@
  * @copyright Copyright 2011-2016 Tilde Inc. and contributors.
  *            Portions Copyright 2011 LivingSocial Inc.
  * @license   Licensed under MIT license (see license.js)
- * @version   2.6.0-canary+7caec86877
+ * @version   2.6.0-canary+ad7c26c60a
  */
 
 var loader, define, requireModule, require, requirejs;
@@ -16039,7 +16039,7 @@ define("ember-data/serializers/rest", ["exports", "ember", "ember-data/-private/
         }
       });
       ```
-       Given a `TacoParty' model, calling `save` on a tacoModel would produce an outgoing
+       Given a `TacoParty` model, calling `save` on it would produce an outgoing
       request like:
        ```js
       {
@@ -16240,7 +16240,7 @@ define('ember-data/transform', ['exports', 'ember'], function (exports, _ember) 
   });
 });
 define("ember-data/version", ["exports"], function (exports) {
-  exports.default = "2.6.0-canary+7caec86877";
+  exports.default = "2.6.0-canary+ad7c26c60a";
 });
 define("ember-inflector", ["exports", "ember", "ember-inflector/lib/system", "ember-inflector/lib/ext/string"], function (exports, _ember, _emberInflectorLibSystem, _emberInflectorLibExtString) {
 
