182f200c26da4f4c156dce382632d4e641350548
Tomster <tomster@emberjs.com>
2016-05-10 07:51:25 +0000

    Ember Data Bower Auto build for https://github.com/emberjs/data/commits/afeb5e052b97b3113d993965d205bb725a742a63.

--- ember-data.prod.js
+++ ember-data.prod.js
@@ -6,7 +6,7 @@
  * @copyright Copyright 2011-2016 Tilde Inc. and contributors.
  *            Portions Copyright 2011 LivingSocial Inc.
  * @license   Licensed under MIT license (see license.js)
- * @version   2.7.0-canary+2001ee3e1d
+ * @version   2.7.0-canary+afeb5e052b
  */
 
 var loader, define, requireModule, require, requirejs;
@@ -255,7 +255,7 @@ var loader, define, requireModule, require, requirejs;
 
   requirejs.clear();
 
-  if (typeof module !== 'undefined') {
+  if (typeof exports === 'object' && typeof module === 'object' && module.exports) {
     module.exports = { require: require, define: define };
   }
 })(this);
@@ -13241,7 +13241,7 @@ define('ember-data/serializers/embedded-records-mixin', ['exports', 'ember', 'em
       if (!embeddedSnapshot) {
         json[serializedKey] = null;
       } else {
-        json[serializedKey] = embeddedSnapshot.record.serialize({ includeId: true });
+        json[serializedKey] = embeddedSnapshot.serialize({ includeId: true });
         this.removeEmbeddedForeignKey(snapshot, embeddedSnapshot, relationship, json[serializedKey]);
 
         if (relationship.options.polymorphic) {
@@ -13419,7 +13419,7 @@ define('ember-data/serializers/embedded-records-mixin', ['exports', 'ember', 'em
 
       for (var i = 0; i < manyArray.length; i++) {
         var embeddedSnapshot = manyArray[i];
-        var embeddedJson = embeddedSnapshot.record.serialize({ includeId: true });
+        var embeddedJson = embeddedSnapshot.serialize({ includeId: true });
         this.removeEmbeddedForeignKey(snapshot, embeddedSnapshot, relationship, embeddedJson);
         ret[i] = embeddedJson;
       }
@@ -16245,7 +16245,7 @@ define('ember-data/transform', ['exports', 'ember'], function (exports, _ember) 
   });
 });
 define("ember-data/version", ["exports"], function (exports) {
-  exports.default = "2.7.0-canary+2001ee3e1d";
+  exports.default = "2.7.0-canary+afeb5e052b";
 });
 define("ember-inflector", ["exports", "ember", "ember-inflector/lib/system", "ember-inflector/lib/ext/string"], function (exports, _ember, _emberInflectorLibSystem, _emberInflectorLibExtString) {
 
