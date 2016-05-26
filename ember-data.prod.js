43a2e89389812d91f82cf4bec04b55d5657060e8
Tomster <tomster@emberjs.com>
2016-05-26 15:48:27 +0000

    Ember Data Bower Auto build for https://github.com/emberjs/data/commits/3b5c5917d6d697b775ebf3e8ac6f454e95dbb63b.

--- ember-data.prod.js
+++ ember-data.prod.js
@@ -6,7 +6,7 @@
  * @copyright Copyright 2011-2016 Tilde Inc. and contributors.
  *            Portions Copyright 2011 LivingSocial Inc.
  * @license   Licensed under MIT license (see license.js)
- * @version   2.7.0-canary+8337df0e36
+ * @version   2.7.0-canary+3b5c5917d6
  */
 
 var loader, define, requireModule, require, requirejs;
@@ -12181,10 +12181,14 @@ define('ember-data/adapters/rest', ['exports', 'ember', 'ember-data/adapter', 'e
       var url = this.buildURL(snapshot.modelName, snapshot.id, snapshot);
 
       var expandedURL = url.split('/');
-      //Case when the url is of the format ...something/:id
+      // Case when the url is of the format ...something/:id
+      // We are decodeURIComponent-ing the lastSegment because if it represents
+      // the id, it has been encodeURIComponent-ified within `buildURL`. If we
+      // don't do this, then records with id having special characters are not
+      // coalesced correctly (see GH #4190 for the reported bug)
       var lastSegment = expandedURL[expandedURL.length - 1];
       var id = snapshot.id;
-      if (lastSegment === id) {
+      if (decodeURIComponent(lastSegment) === id) {
         expandedURL[expandedURL.length - 1] = "";
       } else if (endsWith(lastSegment, '?id=' + id)) {
         //Case when the url is of the format ...something?id=:id
@@ -16721,7 +16725,7 @@ define('ember-data/transform', ['exports', 'ember'], function (exports, _ember) 
   });
 });
 define("ember-data/version", ["exports"], function (exports) {
-  exports.default = "2.7.0-canary+8337df0e36";
+  exports.default = "2.7.0-canary+3b5c5917d6";
 });
 define("ember-inflector", ["exports", "ember", "ember-inflector/lib/system", "ember-inflector/lib/ext/string"], function (exports, _ember, _emberInflectorLibSystem, _emberInflectorLibExtString) {
 
