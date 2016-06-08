ecc2e40d620927cfcbddd7e0a6857bae4919e908
Tomster <tomster@emberjs.com>
2016-06-08 22:19:26 +0000

    Ember Data Bower Auto build for https://github.com/emberjs/data/commits/5cfa6889f4462e7408c29aa51c84772f38f50867.

--- ember-data.prod.js
+++ ember-data.prod.js
@@ -6,7 +6,7 @@
  * @copyright Copyright 2011-2016 Tilde Inc. and contributors.
  *            Portions Copyright 2011 LivingSocial Inc.
  * @license   Licensed under MIT license (see license.js)
- * @version   2.7.0-canary+76f077cbaa
+ * @version   2.7.0-beta.1
  */
 
 var loader, define, requireModule, require, requirejs;
@@ -16483,7 +16483,7 @@ define('ember-data/transform', ['exports', 'ember'], function (exports, _ember) 
   });
 });
 define("ember-data/version", ["exports"], function (exports) {
-  exports.default = "2.7.0-canary+76f077cbaa";
+  exports.default = "2.7.0-beta.1";
 });
 define("ember-inflector", ["exports", "ember", "ember-inflector/lib/system", "ember-inflector/lib/ext/string"], function (exports, _ember, _emberInflectorLibSystem, _emberInflectorLibExtString) {
 
