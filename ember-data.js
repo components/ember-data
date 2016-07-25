3ec7568e2bcfdc78e5dfc12ef281cb3cc6914fc0
bmac <bmac325@gmail.com>
2016-07-25 16:35:39 -0400

    Release Ember Data 2.5.5

--- ember-data.js
+++ ember-data.js
@@ -6,7 +6,7 @@
  * @copyright Copyright 2011-2016 Tilde Inc. and contributors.
  *            Portions Copyright 2011 LivingSocial Inc.
  * @license   Licensed under MIT license (see license.js)
- * @version   2.5.4
+ * @version   2.5.5
  */
 
 var define, requireModule, require, requirejs;
@@ -15607,7 +15607,7 @@ define('ember-data/transform', ['exports', 'ember'], function (exports, _ember) 
   });
 });
 define("ember-data/version", ["exports"], function (exports) {
-  exports.default = "2.5.4";
+  exports.default = "2.5.5";
 });
 define("ember-inflector", ["exports", "ember", "ember-inflector/lib/system", "ember-inflector/lib/ext/string"], function (exports, _ember, _emberInflectorLibSystem, _emberInflectorLibExtString) {
 
