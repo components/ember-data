8f5ae23fdf7e3a2cc78a5c6ebb7a10313ec7ceed
Tomster <tomster@emberjs.com>
2016-08-25 13:58:24 +0000

    Ember Data Bower Auto build for https://github.com/emberjs/data/commits/b87a82d77828475e407ee76d4136d44e48ccbc0d.

--- ember-data.prod.js
+++ ember-data.prod.js
@@ -6,7 +6,7 @@
  * @copyright Copyright 2011-2016 Tilde Inc. and contributors.
  *            Portions Copyright 2011 LivingSocial Inc.
  * @license   Licensed under MIT license (see license.js)
- * @version   2.8.0-canary+b3dabc823b
+ * @version   2.8.0-canary+b87a82d778
  */
 
 var loader, define, requireModule, require, requirejs;
@@ -10486,7 +10486,8 @@ define("ember-data/-private/transforms/date", ["exports", "ember-data/-private/e
   The `DS.DateTransform` class is used to serialize and deserialize
   date attributes on Ember Data record objects. This transform is used
   when `date` is passed as the type parameter to the
-  [DS.attr](../../data#method_attr) function.
+  [DS.attr](../../data#method_attr) function. It uses the [`ISO 8601`](https://en.wikipedia.org/wiki/ISO_8601)
+  standard.
 
   ```app/models/score.js
   import DS from 'ember-data';
@@ -16997,7 +16998,7 @@ define('ember-data/transform', ['exports', 'ember'], function (exports, _ember) 
   });
 });
 define("ember-data/version", ["exports"], function (exports) {
-  exports.default = "2.8.0-canary+b3dabc823b";
+  exports.default = "2.8.0-canary+b87a82d778";
 });
 define("ember-inflector", ["exports", "ember", "ember-inflector/lib/system", "ember-inflector/lib/ext/string"], function (exports, _ember, _emberInflectorLibSystem, _emberInflectorLibExtString) {
 
