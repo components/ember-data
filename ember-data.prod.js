e2bf9924e31fac3c2dfd2a2c51477a7914c524b5
bmac <bmac325@gmail.com>
2016-08-29 08:41:44 -0400

    Release Ember Data 2.8.0-beta.3

--- ember-data.prod.js
+++ ember-data.prod.js
@@ -6,7 +6,7 @@
  * @copyright Copyright 2011-2016 Tilde Inc. and contributors.
  *            Portions Copyright 2011 LivingSocial Inc.
  * @license   Licensed under MIT license (see license.js)
- * @version   2.8.0-beta.2
+ * @version   2.8.0-beta.3
  */
 
 var loader, define, requireModule, require, requirejs;
@@ -5471,7 +5471,7 @@ define("ember-data/-private/system/record-arrays/record-array", ["exports", "emb
       var content = get(this, 'content');
       if (idx === undefined) {
         content.addObject(internalModel);
-      } else if (!content.contains(internalModel)) {
+      } else if (!content.includes(internalModel)) {
         content.insertAt(idx, internalModel);
       }
     },
@@ -6029,7 +6029,7 @@ define("ember-data/-private/system/relationships/ext", ["exports", "ember", "emb
         meta.key = name;
         modelName = (0, _emberDataPrivateSystemRelationshipMeta.typeForRelationshipMeta)(meta);
 
-        if (!types.contains(modelName)) {
+        if (!types.includes(modelName)) {
           types.push(modelName);
         }
       }
@@ -8459,7 +8459,7 @@ define('ember-data/-private/system/store', ['exports', 'ember', 'ember-data/mode
         return function rejectMissingRecords(resolvedRecords) {
           resolvedRecords = _ember.default.A(resolvedRecords);
           var missingRecords = requestedRecords.reject(function (record) {
-            return resolvedRecords.contains(record);
+            return resolvedRecords.includes(record);
           });
           if (missingRecords.length) {}
           rejectRecords(missingRecords);
@@ -16997,7 +16997,7 @@ define('ember-data/transform', ['exports', 'ember'], function (exports, _ember) 
   });
 });
 define("ember-data/version", ["exports"], function (exports) {
-  exports.default = "2.8.0-beta.2";
+  exports.default = "2.8.0-beta.3";
 });
 define("ember-inflector", ["exports", "ember", "ember-inflector/lib/system", "ember-inflector/lib/ext/string"], function (exports, _ember, _emberInflectorLibSystem, _emberInflectorLibExtString) {
 
