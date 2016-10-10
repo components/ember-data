3177360428133007235ba80780187f751b0efcf1
Tomster <tomster@emberjs.com>
2016-10-10 14:12:07 +0000

    Ember Data Bower Auto build for https://github.com/emberjs/data/commits/9f6d4e1b2a5cc8010046dd6a2e94a921a53ee743.

--- ember-data.js
+++ ember-data.js
@@ -6,7 +6,7 @@
  * @copyright Copyright 2011-2016 Tilde Inc. and contributors.
  *            Portions Copyright 2011 LivingSocial Inc.
  * @license   Licensed under MIT license (see license.js)
- * @version   2.9.0-beta.3
+ * @version   2.9.0-beta.3+9f6d4e1b2a
  */
 
 var loader, define, requireModule, require, requirejs;
@@ -9081,15 +9081,15 @@ define('ember-data/-private/system/store', ['exports', 'ember', 'ember-data/mode
       (0, _emberDataPrivateDebug.assert)("You tried to load all records but you have no adapter (for " + typeClass + ")", adapter);
       (0, _emberDataPrivateDebug.assert)("You tried to load all records but your adapter does not implement `findAll`", typeof adapter.findAll === 'function');
 
-      set(array, 'isUpdating', true);
-
       if (options.reload) {
+        set(array, 'isUpdating', true);
         return (0, _emberDataPrivateSystemPromiseProxies.promiseArray)((0, _emberDataPrivateSystemStoreFinders._findAll)(adapter, this, typeClass, sinceToken, options));
       }
 
       var snapshotArray = array.createSnapshot(options);
 
       if (adapter.shouldReloadAll(this, snapshotArray)) {
+        set(array, 'isUpdating', true);
         return (0, _emberDataPrivateSystemPromiseProxies.promiseArray)((0, _emberDataPrivateSystemStoreFinders._findAll)(adapter, this, typeClass, sinceToken, options));
       }
 
@@ -9098,6 +9098,7 @@ define('ember-data/-private/system/store', ['exports', 'ember', 'ember-data/mode
       }
 
       if (options.backgroundReload || adapter.shouldBackgroundReloadAll(this, snapshotArray)) {
+        set(array, 'isUpdating', true);
         (0, _emberDataPrivateSystemStoreFinders._findAll)(adapter, this, typeClass, sinceToken, options);
       }
 
@@ -13234,7 +13235,7 @@ define('ember-data/adapters/rest', ['exports', 'ember', 'ember-data/adapter', 'e
       error = responseData.errorThrown;
     } else if (responseData.textStatus === 'timeout') {
       error = new _emberDataAdaptersErrors.TimeoutError();
-    } else if (responseData.textStatus === 'abort') {
+    } else if (responseData.textStatus === 'abort' || jqXHR.status === 0) {
       error = new _emberDataAdaptersErrors.AbortError();
     } else {
       try {
@@ -17478,7 +17479,7 @@ define('ember-data/transform', ['exports', 'ember'], function (exports, _ember) 
   });
 });
 define("ember-data/version", ["exports"], function (exports) {
-  exports.default = "2.9.0-beta.3";
+  exports.default = "2.9.0-beta.3+9f6d4e1b2a";
 });
 define("ember-inflector", ["exports", "ember", "ember-inflector/lib/system", "ember-inflector/lib/ext/string"], function (exports, _ember, _emberInflectorLibSystem, _emberInflectorLibExtString) {
 
