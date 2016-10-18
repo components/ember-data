be1e5abb455bce595c14eec5a3e10058434eeb35
Tomster <tomster@emberjs.com>
2016-10-18 18:01:28 +0000

    Ember Data Bower Auto build for https://github.com/emberjs/data/commits/0cd85e7787ad4d829c1839f6d391d151cfcadd4e.

--- ember-data.prod.js
+++ ember-data.prod.js
@@ -6,7 +6,7 @@
  * @copyright Copyright 2011-2016 Tilde Inc. and contributors.
  *            Portions Copyright 2011 LivingSocial Inc.
  * @license   Licensed under MIT license (see license.js)
- * @version   2.11.0-canary+88e909cdef
+ * @version   2.11.0-canary+0cd85e7787
  */
 
 var loader, define, requireModule, require, requirejs;
@@ -5294,7 +5294,7 @@ define("ember-data/-private/system/record-arrays", ["exports", "ember-data/-priv
 /**
   @module ember-data
 */
-define("ember-data/-private/system/record-arrays/adapter-populated-record-array", ["exports", "ember", "ember-data/-private/system/record-arrays/record-array", "ember-data/-private/system/clone-null"], function (exports, _ember, _emberDataPrivateSystemRecordArraysRecordArray, _emberDataPrivateSystemCloneNull) {
+define("ember-data/-private/system/record-arrays/adapter-populated-record-array", ["exports", "ember", "ember-data/-private/system/record-arrays/record-array", "ember-data/-private/system/clone-null", "ember-data/-private/features"], function (exports, _ember, _emberDataPrivateSystemRecordArraysRecordArray, _emberDataPrivateSystemCloneNull, _emberDataPrivateFeatures) {
 
   /**
     @module ember-data
@@ -5346,7 +5346,9 @@ define("ember-data/-private/system/record-arrays/adapter-populated-record-array"
         meta: (0, _emberDataPrivateSystemCloneNull.default)(payload.meta)
       });
 
-      this.set('links', (0, _emberDataPrivateSystemCloneNull.default)(payload.links));
+      if (true) {
+        this.set('links', (0, _emberDataPrivateSystemCloneNull.default)(payload.links));
+      }
 
       internalModels.forEach(function (record) {
         _this.manager.recordArraysForRecord(record).add(_this);
@@ -17318,7 +17320,7 @@ define('ember-data/transform', ['exports', 'ember'], function (exports, _ember) 
   });
 });
 define("ember-data/version", ["exports"], function (exports) {
-  exports.default = "2.11.0-canary+88e909cdef";
+  exports.default = "2.11.0-canary+0cd85e7787";
 });
 define("ember-inflector", ["exports", "ember", "ember-inflector/lib/system", "ember-inflector/lib/ext/string"], function (exports, _ember, _emberInflectorLibSystem, _emberInflectorLibExtString) {
 
