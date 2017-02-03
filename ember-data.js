c9b59f821267ef0e71a1002b00513215f8347028
Tomster <tomster@emberjs.com>
2017-02-03 21:26:16 +0000

    Ember Data Bower Auto build for https://github.com/emberjs/data/commits/4fa27757b00ad2722971d9852036602fa90b6be0.

--- ember-data.js
+++ ember-data.js
@@ -6,7 +6,7 @@
  * @copyright Copyright 2011-2016 Tilde Inc. and contributors.
  *            Portions Copyright 2011 LivingSocial Inc.
  * @license   Licensed under MIT license (see license.js)
- * @version   2.11.0+1803e97a5b
+ * @version   2.11.0+4fa27757b0
  */
 
 var loader, define, requireModule, require, requirejs;
@@ -1482,7 +1482,12 @@ define("ember-data/-private/system/many-array", ["exports", "ember", "ember-data
     flushCanonical: function () {
       var isInitialized = arguments.length <= 0 || arguments[0] === undefined ? true : arguments[0];
 
-      var toSet = this.canonicalState;
+      // TODO make this smarter, currently its plenty stupid
+      // TODO this filtering was re-introduced as a bugfix, but seems unneeded in 2.13
+      // with the changes to internalModel cleanup in that version.
+      var toSet = this.canonicalState.filter(function (internalModel) {
+        return internalModel.currentState.stateName !== 'root.deleted.saved';
+      });
 
       //a hack for not removing new records
       //TODO remove once we have proper diffing
@@ -19122,7 +19127,7 @@ define('ember-data/transform', ['exports', 'ember'], function (exports, _ember) 
   });
 });
 define("ember-data/version", ["exports"], function (exports) {
-  exports.default = "2.11.0+1803e97a5b";
+  exports.default = "2.11.0+4fa27757b0";
 });
 define("ember-inflector", ["exports", "ember", "ember-inflector/lib/system", "ember-inflector/lib/ext/string"], function (exports, _ember, _emberInflectorLibSystem, _emberInflectorLibExtString) {
 
