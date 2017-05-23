2aa23684406aa9c9f5d76a2c15ccb1d2508a7613
Tomster <tomster@emberjs.com>
2017-05-23 17:49:51 +0000

    Ember Data Bower Auto build for https://github.com/emberjs/data/commits/69cdc9e6373409b5fd5e0c248a7aebd3283773aa.

--- ember-data.js
+++ ember-data.js
@@ -6,7 +6,7 @@
  * @copyright Copyright 2011-2017 Tilde Inc. and contributors.
  *            Portions Copyright 2011 LivingSocial Inc.
  * @license   Licensed under MIT license (see license.js)
- * @version   2.15.0-canary+58bd396007
+ * @version   2.15.0-canary+69cdc9e637
  */
 
 var loader, define, requireModule, require, requirejs;
@@ -1758,8 +1758,8 @@ define("ember-data/-private/system/diff-array", ["exports"], function (exports) 
   exports.default = diffArray;
   /**
     @namespace
-    @method diff-array
-    @for DS
+    @method diffArray
+    @private
     @param {Array} oldArray the old array
     @param {Array} newArray the new array
     @return {hash} {
@@ -18275,7 +18275,7 @@ define("ember-data/version", ["exports"], function (exports) {
   "use strict";
 
   exports.__esModule = true;
-  exports.default = "2.15.0-canary+58bd396007";
+  exports.default = "2.15.0-canary+69cdc9e637";
 });
 define("ember-inflector", ["module", "exports", "ember", "ember-inflector/lib/system", "ember-inflector/lib/ext/string"], function (module, exports, _ember, _system) {
   "use strict";
