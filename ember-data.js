562169acb9a6583ccca4b4eeb4f0ce2b3bc47f3f
Stefan Penner <stefan.penner@gmail.com>
2017-07-21 09:03:16 -0700

    Release ember Data 2.14.6

--- ember-data.js
+++ ember-data.js
@@ -6,7 +6,7 @@
  * @copyright Copyright 2011-2017 Tilde Inc. and contributors.
  *            Portions Copyright 2011 LivingSocial Inc.
  * @license   Licensed under MIT license (see license.js)
- * @version   2.14.5+9ddac1a7c6
+ * @version   2.14.6
  */
 
 var loader, define, requireModule, require, requirejs;
@@ -18251,7 +18251,7 @@ define("ember-data/version", ["exports"], function (exports) {
   "use strict";
 
   exports.__esModule = true;
-  exports.default = "2.14.5+9ddac1a7c6";
+  exports.default = "2.14.6";
 });
 define("ember-inflector", ["module", "exports", "ember", "ember-inflector/lib/system", "ember-inflector/lib/ext/string"], function (module, exports, _ember, _system) {
   "use strict";
