ba76d8c503953b493bfd64a7db1929592858a39e
Stefan Penner <stefan.penner@gmail.com>
2017-07-25 14:58:05 -0700

    release v2.14.7

--- ember-data.prod.js
+++ ember-data.prod.js
@@ -6,7 +6,7 @@
  * @copyright Copyright 2011-2017 Tilde Inc. and contributors.
  *            Portions Copyright 2011 LivingSocial Inc.
  * @license   Licensed under MIT license (see license.js)
- * @version   2.14.6+ffbcc42fcd
+ * @version   2.14.7
  */
 
 var loader, define, requireModule, require, requirejs;
@@ -17756,7 +17756,7 @@ define("ember-data/version", ["exports"], function (exports) {
   "use strict";
 
   exports.__esModule = true;
-  exports.default = "2.14.6+ffbcc42fcd";
+  exports.default = "2.14.7";
 });
 define("ember-inflector", ["module", "exports", "ember", "ember-inflector/lib/system", "ember-inflector/lib/ext/string"], function (module, exports, _ember, _system) {
   "use strict";
