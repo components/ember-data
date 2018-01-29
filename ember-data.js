ce8cc6d36efb53197b868b10313ce0a5ce647e7f
Tomster <tomster@emberjs.com>
2018-01-29 20:49:33 +0000

    Ember Data Bower Auto build for https://github.com/emberjs/data/commits/ab4dff563000f1ba9112ec2886cc8c6a8652a803.

--- ember-data.js
+++ ember-data.js
@@ -6,7 +6,7 @@
  * @copyright Copyright 2011-2017 Tilde Inc. and contributors.
  *            Portions Copyright 2011 LivingSocial Inc.
  * @license   Licensed under MIT license (see license.js)
- * @version   3.1.0-canary+4734648427
+ * @version   3.1.0-canary+ab4dff5630
  */
 
 var loader, define, requireModule, require, requirejs;
@@ -18112,54 +18112,40 @@ define("ember-data/version", ["exports"], function (exports) {
   "use strict";
 
   exports.__esModule = true;
-  exports.default = "3.1.0-canary+4734648427";
+  exports.default = "3.1.0-canary+ab4dff5630";
 });
-define('ember-inflector', ['exports', 'ember-inflector/lib/system', 'ember-inflector/lib/ext/string'], function (exports, _system) {
-  'use strict';
+define("ember-inflector", ["module", "exports", "ember-inflector/lib/system", "ember-inflector/lib/ext/string"], function (module, exports, _system) {
+  "use strict";
 
   exports.__esModule = true;
   exports.defaultRules = exports.singularize = exports.pluralize = undefined;
 
 
   _system.Inflector.defaultRules = _system.defaultRules;
+  Ember.Inflector = _system.Inflector;
 
-  Object.defineProperty(Ember, 'Inflector', {
-    get: function () {
-      Ember.deprecate('Ember.Inflector is deprecated. Please explicitly: import Inflector from \'ember-inflector\';', false, {
-        id: 'ember-inflector.globals',
-        until: '3.0.0'
-      });
-
-      return _system.Inflector;
-    }
-  });
-
-  Object.defineProperty(Ember.String, 'singularize', {
-    get: function () {
-      Ember.deprecate('Ember.String.singularize() is deprecated. Please explicitly: import { singularize } from \'ember-inflector\';', false, {
-        id: 'ember-inflector.globals',
-        until: '3.0.0'
-      });
-
-      return _system.singularize;
-    }
-  });
-
-  Object.defineProperty(Ember.String, 'pluralize', {
-    get: function () {
-      Ember.deprecate('Ember.String.pluralize() is deprecated. Please explicitly: import { pluralize } from \'ember-inflector\';', false, {
-        id: 'ember-inflector.globals',
-        until: '3.0.0'
-      });
-
-      return _system.pluralize;
-    }
-  });
+  Ember.String.pluralize = _system.pluralize;
+  Ember.String.singularize = _system.singularize;
 
   exports.default = _system.Inflector;
   exports.pluralize = _system.pluralize;
   exports.singularize = _system.singularize;
   exports.defaultRules = _system.defaultRules;
+
+
+  if (typeof define !== 'undefined' && define.amd) {
+    define('ember-inflector', ['exports'], function (__exports__) {
+      __exports__['default'] = _system.Inflector;
+      __exports__.pluralize = _system.pluralize;
+      __exports__.singularize = _system.singularize;
+
+      return __exports__;
+    });
+  } else if (typeof module !== 'undefined' && module['exports']) {
+    module['exports'] = _system.Inflector;
+    _system.Inflector.singularize = _system.singularize;
+    _system.Inflector.pluralize = _system.pluralize;
+  }
 });
 define('ember-inflector/lib/ext/string', ['ember-inflector/lib/system/string'], function (_string) {
   'use strict';
@@ -18170,36 +18156,18 @@ define('ember-inflector/lib/ext/string', ['ember-inflector/lib/system/string'], 
        @method pluralize
       @for String
     */
-    Object.defineProperty(String.prototype, 'pluralize', {
-      get: function () {
-        Ember.deprecate('String.prototype.pluralize() is deprecated. Please explicitly: import { pluralize } from \'ember-inflector\';', false, {
-          id: 'ember-inflector.globals',
-          until: '3.0.0'
-        });
-
-        return function () {
-          return (0, _string.pluralize)(this);
-        };
-      }
-    });
+    String.prototype.pluralize = function () {
+      return (0, _string.pluralize)(this);
+    };
 
     /**
       See {{#crossLink "Ember.String/singularize"}}{{/crossLink}}
        @method singularize
       @for String
     */
-    Object.defineProperty(String.prototype, 'singularize', {
-      get: function () {
-        Ember.deprecate('String.prototype.singularize() is deprecated. Please explicitly: import { singularize } from \'ember-inflector\';', false, {
-          id: 'ember-inflector.globals',
-          until: '3.0.0'
-        });
-
-        return function () {
-          return (0, _string.singularize)(this);
-        };
-      }
-    });
+    String.prototype.singularize = function () {
+      return (0, _string.singularize)(this);
+    };
   }
 });
 define('ember-inflector/lib/helpers/pluralize', ['exports', 'ember-inflector', 'ember-inflector/lib/utils/make-helper'], function (exports, _emberInflector, _makeHelper) {
